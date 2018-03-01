1de07880fabb4da8fdf523c345f3f39980546233
WojciechO <wojciech.orchowskI@gmail.com>
2018-03-01 20:13:12 +0100

    Starting Info

--- src/main/java/turbo/Main.java
+++ src/main/java/turbo/Main.java
@@ -12,6 +12,10 @@ public class Main {
         Integer t = 10;
 
         Ride wojtek = Ride.builder().earlierStart(2).latestFinish(9).startPoint(new Pair<>(0, 0)).finishPoint(new Pair<>(1, 3)).build();
+        StartingInfo startinginfo = StartingInfo.builder().bonus(2).column(4).fleet(2).numberOfRides(3).row(3).time(10).build();
+
+                
+
 
         while (time < t) {
 
