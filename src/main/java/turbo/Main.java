0b99f45abd8b42dc9ad9e77f86f6b2bbfbd13547
Rafal Nowakowski <rafal1nowakowski@gmail.com>
2018-03-01 20:21:40 +0100

    updat

--- src/main/java/turbo/Main.java
+++ src/main/java/turbo/Main.java
@@ -5,6 +5,10 @@ import turbo.parser.FileReader;
 
 import java.io.IOException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Qenlin on 01.03.2018.
  */
@@ -18,9 +22,8 @@ public class Main {
         Ride ride2 = Ride.builder().earlierStart(0).latestFinish(9).startPoint(new Pair<>(1, 2)).finishPoint(new Pair<>(1, 0)).build();
         Ride ride3 = Ride.builder().earlierStart(0).latestFinish(9).startPoint(new Pair<>(2, 0)).finishPoint(new Pair<>(2, 2)).build();
 
-        StartingInfo startinginfo = StartingInfo.builder().bonus(2).column(4).fleet(2).numberOfRides(3).row(3).time(10).build();
+        StartingInfo startinginfo = StartingInfo.builder().bonus(2).column(4).fleet(2).numberOfRides(3).row(3).time(10).rides(Arrays.asList(ride1,ride2,ride3)).build();
 
-        
         while (time < t) {
 
         }
