97e3c64cc25ac8345faf2f486d9f33d8403a68c0
Baranowski Kamil <0baranowski.kamil@gmail.com>
2018-03-01 19:35:02 +0100

    Lombok added Ride object added

---
+++ src/main/java/Ride.java
@@ -0,0 +1,23 @@
+import javafx.util.Pair;
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Qenlin on 01.03.2018.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class Ride {
+    private Integer earlierStart;
+    private Integer latestFinish;
+    private Pair<Integer, Integer> startPoint;
+    private Pair<Integer, Integer> finishPoint;
+
+    public Integer getDistance(){
+
+        return 
+    }
+}
