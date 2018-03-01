174a52be3333abc2d6c5102835e2ee2cd3526a60
Rafal Nowakowski <rafal1nowakowski@gmail.com>
2018-03-01 20:10:48 +0100

    rides

--- src/main/java/turbo/Main.java
+++ src/main/java/turbo/Main.java
@@ -11,7 +11,10 @@ public class Main {
         Integer time = 0;
         Integer t = 10;
 
-        Ride wojtek = Ride.builder().earlierStart(2).latestFinish(9).startPoint(new Pair<>(0, 0)).finishPoint(new Pair<>(1, 3)).build();
+        Ride ride1 = Ride.builder().earlierStart(2).latestFinish(9).startPoint(new Pair<>(0, 0)).finishPoint(new Pair<>(1, 3)).build();
+        Ride ride2 = Ride.builder().earlierStart(0).latestFinish(9).startPoint(new Pair<>(1, 2)).finishPoint(new Pair<>(1, 0)).build();
+        Ride ride3 = Ride.builder().earlierStart(0).latestFinish(9).startPoint(new Pair<>(2, 0)).finishPoint(new Pair<>(2, 2)).build();
+
 
         while (time < t) {
 
